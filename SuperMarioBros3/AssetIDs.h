c9679a45a8542fa38a10d7a865136a49816123b2
nguyenhun11 <giahungcantho11@gmail.com>
2026-04-26 23:51:43 +0700

    feat: create texture id

--- SuperMarioBros3/AssetIDs.h
+++ SuperMarioBros3/AssetIDs.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 //
 // This file contains all IDs of game assets 
@@ -8,13 +8,26 @@
 #define ID_TEX_ENEMY 10
 #define ID_TEX_MISC 20
 
+/*
+	Các bước thêm mới:
+	1. define trong đây con số nào cũng được, không trùng những cái trước
+	2. Đổi tên file gốc để dễ biết
+	3. Thêm số tương ứng vào game-data.txt
+
+*/
+#define ID_TEXTURE_MARIO			100
+#define ID_TEXTURE_ENEMIES			200
+#define ID_TEXTURE_TILESETS_STAGE	300
+#define ID_TEXTURE_ITEMS			400
+
+
 
 #define OBJECT_TYPE_MARIO	0
 #define OBJECT_TYPE_BRICK	1
 #define OBJECT_TYPE_GOOMBA	2
 #define OBJECT_TYPE_KOOPAS	3
 #define OBJECT_TYPE_COIN 4
-#define OBJECT_TYPE_PLATFORM 5
+#define OBJECT_TYPE_PLATFORM 7
 
 #define OBJECT_TYPE_PORTAL	50
 
